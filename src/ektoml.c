ae7e64ee5b3c837391bce46afbb723b88f14b155
Wyatt Radkiewicz <wyattwradkiewicz@gmail.com>
2024-09-17 22:47:41 -0600

    Finished utf8 parser

--- src/ektoml.c
+++ src/ektoml.c
@@ -1,76 +1,193 @@
+#include <alloca.h>
+#include <ctype.h>
+#include <string.h>
+
 #include "ektoml.h"
 
-// Helper defines to return an error or okay value
-#define TOML_OK ((toml_result_t){ .okay = true })
-#define TOML_ERR(_state, _in, _msg) ((toml_result_t){ \
-	.okay = false, \
-	.line = (_state)->line, \
-	.in = (_in), \
-	.msg = (_msg), \
-})
+#define SET_ERR(_parser, _msg) ((_parser)->res = (toml_res_t){ \
+		.ok = false, \
+		.line = (_parser)->line, .pos = (_parser)->pos, \
+		.msg = (_msg) \
+	})
 
-// NOTE: This check macro depends on depth, state, and an optional table value
-// It also requries that the function returns 
-#define TOML_CHECK_DEPTH(_table) \
-	if (depth >= TOML_MAX_RECURSION_DEPTH) { \
-		return TOML_ERR(state, (_table), "hit recursion depth limit"); \
-	}
+typedef struct parser parser_t;
 
-// Monolithic struct for parsing
-typedef struct parser_state {
-	// Where we are in the source bytes wise
-	const uint8_t *src;
+struct parser {
+	const char *src;
+	uint32_t line, pos;
 
-	// What line we're on
-	uint32_t line, col;
+	toml_res_t res;
 
-	// The root node
-	const toml_t *root;
+	toml_table_info_t *root;
 
-	// The user variable passed in (passed to setter funcs)
-	void *user;
+	uint8_t *arena_ptr, *arena_end;
+};
 
-	// What 'has been parsed flag' generation we are on
-	uint16_t gen;
-
-	// Temporary storage for key names
-	uint8_t key_buf[TOML_MAX_KEY_SIZE];
-} parser_state_t;
-
-static toml_result_t toml_parse_key(parser_state_t *const state) {
-	return TOML_OK;
+static void set_err_oom(parser_t *parser) {
+	SET_ERR(parser, "out of arena memory");
 }
 
-static toml_result_t toml_dotable(parser_state_t *const state,
-					const toml_t *table,
-					const uint32_t depth) {
-	TOML_CHECK_DEPTH(table)
-
-
-	return TOML_OK;
+static void parse_whitespace(parser_t *parser, bool skip_newline) {
+	while (*parser->src == ' ' || *parser->src == '\t'
+		|| skip_newline && *parser->src == '\n') {
+		parser->pos++;
+		if (*parser->src++ == '\n') {
+			parser->line++;
+			parser->pos = 1;
+		}
+	}
 }
 
-toml_result_t toml_parse(const toml_args_t args) {
-	const toml_t root_node = {
-		.val = {
-			.type = TOML_TABLE,
-			.isoptional = false,
-			.data.table_start = args.root_table,
-			.len = args.root_len,
-		},
-	};
-	parser_state_t state = {
-		.src = args.src,
-		.line = 1,
-		.user = args.user,
-		.root = &root_node,
-		.gen = *args.gen,
-	};
+// The length of utf8 character
+// Returns 0 if the first character is invalid
+static inline uint32_t utf8_header_char_length(const char start) {
+	const uint32_t clz = __builtin_clz(~(uint8_t)start << 24);
+	const uint32_t leading_len = clz + !clz;
+	if (clz == 1 || leading_len > 4) return 0;
+	return leading_len;
+}
 
-	if (args.root_len >= 1ull << (sizeof(root_node.val.len) * 8)) {
-		return TOML_ERR(&state, NULL, "root node length too big");
+// Should be called on every utf8-character
+static inline bool utf8_arbitrary_byte_valid(const char c) {
+	switch ((uint8_t)c) {
+	case 0xC0: case 0xC1:
+	case 0xF5: case 0xF6: case 0xF7: case 0xF8: case 0xF9:
+	case 0xFA: case 0xFB: case 0xFC: case 0xFD: case 0xFE: case 0xFF:
+		return false;
+	default:
+		return true;
+	}
+}
+
+static inline bool utf8_is_continue_byte(const char c) {
+	return (uint8_t)c >= 0x80 && (uint8_t)c <= 0xBF;
+}
+
+// Validate UTF-8 characters coming in (per-spec ofc)
+// and push them onto the arena
+// Returns whether or not it could allocate memory for the character
+// If an invalid UTF-8 character is found, it will deal with it and print
+// out an appropiate replacement character ('�' or U+FFFD in this instance)
+static bool parse_char(parser_t *parser) {
+	uint8_t *const mem_start = parser->arena_ptr;
+	const char *const src_start = parser->src;
+	uint32_t len = utf8_header_char_length(*parser->src);
+	uint32_t codepoint = 0;
+	if (!len || !utf8_arbitrary_byte_valid(*parser->src)) goto err;
+
+	// Make sure we have enough memory for this character
+	if (parser->arena_ptr + len > parser->arena_end) {
+		set_err_oom(parser);
+		return false;
 	}
 
-	return toml_dotable(&state, &root_node, 1);
+	// Validate and get first byte
+	switch (len--) {
+	case 4: codepoint |= (*parser->src & 0x07) << 18; break;
+	case 3: codepoint |= (*parser->src & 0x0F) << 12; break;
+	case 2: codepoint |= (*parser->src & 0x1F) << 6; break;
+	case 1: codepoint = *parser->src; break;
+	}
+	*parser->arena_ptr++ = *parser->src++;
+	if (!len) return true;
+
+	// Validate continuation bytes
+	for (; len; len--) {
+		if (*parser->src == '\0') goto err;
+		if (!utf8_is_continue_byte(*parser->src)) goto err;
+		if (!utf8_arbitrary_byte_valid(*parser->src)) goto err;
+		codepoint |= (*parser->src & 0x3F) << (len * 6);
+		*parser->arena_ptr++ = *parser->src++;
+	}
+
+	// Validate codepoint
+	if (codepoint > 0x10FFFF || codepoint > 0xD800 && codepoint <= 0xDFFF) goto err;
+	return true;
+
+err:
+	parser->arena_ptr = mem_start;
+	parser->src = src_start + 1;
+	if (parser->arena_ptr + 3 > parser->arena_end) {
+		set_err_oom(parser);
+		return false;
+	}
+	memcpy(parser->arena_ptr, (char []){'\xEF', '\xBF', '\xBD'}, 3);
+	parser->arena_ptr += 3;
+
+	return false;
+}
+
+static char *parse_string(parser_t *parser) {
+	const char start = *parser->src;
+	const bool is_multiline = parser->src[0] == parser->src[1]
+				&& parser->src[0] == parser->src[2];
+	char *const str = (char *)parser->arena_ptr;
+
+	parser->src += is_multiline ? 3 : 1;
+	parser->pos += is_multiline ? 3 : 1;
+
+	while (*parser->src != start) {
+		if (!parse_char(parser)) return NULL;
+	}
+	if (parser->arena_ptr >= parser->arena_end) {
+		set_err_oom(parser);
+		return NULL;
+	}
+	*parser->arena_ptr++ = '\0';
+
+	if (is_multiline) {
+		if (parser->src[1] != start || parser->src[2] != start) {
+			SET_ERR(parser, "string end sequence does not match start");
+			return NULL;
+		}
+		parser->src += 3, parser->pos += 3;
+	} else {
+		parser->src += 1, parser->pos += 1;
+	}
+
+	return str;
+}
+
+static toml_table_info_t *add_table_info(parser_t *parser,
+					const toml_table_info_t info) {
+	toml_table_info_t *table = (toml_table_info_t *)parser->arena_ptr;
+	parser->arena_ptr += sizeof(*table) + info.len * sizeof(void *);
+	if (parser->arena_ptr >= parser->arena_end) {
+		set_err_oom(parser);
+		return NULL;
+	}
+	*table = info;
+	memset(table->_kv, 0, sizeof(*table->_kv) * table->len);
+
+	return table;
+}
+
+// Skip byte-order mark (BOM) for src since its UTF-8 and doesn't need a BOM
+static void skip_bom(parser_t *parser) {
+	if (strncmp(parser->src, (char []){'\xEF', '\xBB', '\xBF'}, 3) == 0) {
+		parser->src += 3;
+	}
+}
+
+toml_res_t toml_parse(const char *src, const toml_table_info_t schema_root,
+			void *arena, size_t arena_len) {
+	if (!arena_len) arena_len = 1024 * 4;
+	if (!arena) arena = alloca(arena_len);
+
+	parser_t *const parser = arena;
+	*parser = (parser_t){
+		.src = src,
+		.line = 1, .pos = 1,
+	
+		.res = { .ok = true },
+
+		.arena_ptr = (uint8_t *)(parser + 1),
+		.arena_end = arena + arena_len / sizeof(void *),
+	};
+	skip_bom(parser);
+	if (!(parser->root = add_table_info(parser, schema_root))) goto end;
+
+end:
+	return parser->res;
 }
 
--- src/ektoml.h
+++ src/ektoml.h
@@ -6,180 +6,107 @@
 #include <stdint.h>
 #include <time.h>
 
-// How many deep recursive functions in ektoml can go
-// This is NOT the same as call stack depth, non recursive
-// functions can make the call stack deeper than this limit
-#define TOML_MAX_RECURSION_DEPTH 64
-
-// See structure definition below for details
-typedef struct toml toml_t;
-
-// All the different types in a toml file
-// TY(enum name, code name, type)
-#define TOML_TYPES \
-	/* NOTE: String is CONST qualified in code */ \
-	TY(TOML_STRING, string, char *) \
-	TY(TOML_INT, int, int64_t) \
-	TY(TOML_FLOAT, float, double) \
-	TY(TOML_BOOL, bool, bool) \
-	TY(TOML_DATETIME_UNIX, datetime_unix, struct timespec) \
-	TY(TOML_DATETIME_LOCAL, datetime_local, toml_datetime_local_t) \
-	TY(TOML_DATE_LOCAL, date_local, struct tm) \
-	TY(TOML_TIME_LOCAL, time_local, toml_time_local_t) \
-	TY_SPECIAL(TOML_ARRAY) \
-	TY_SPECIAL(TOML_TABLE)
-
-// An eunmeration of different types in a toml file
 typedef enum toml_type {
-#define TY(ename, cname, ty) ename,
-#define TY_SPECIAL(ename) ename,
-	TOML_TYPES
-#undef TY
-#undef TY_SPECIAL
+	TOML_STRING,
+	TOML_INT,
+	TOML_FLOAT,
+	TOML_BOOL,
+	TOML_DATETIME_UNIX,
+	TOML_DATETIME,
+	TOML_DATE,
+	TOML_TIME,
+	TOML_ARRAY,
+	TOML_TABLE,
+	TOML_ANY,
 } toml_type_t;
 
-// Represents a local time with no offset from an absolute time (like UTC)
-// Actual time is interpreted by user or system
-typedef struct toml_time_local {
-	// Hour and minute of unspecified day
-	uint8_t hour, min;
-
-	// Seconds into the minute plus nanoseconds
-	struct timespec sec;
-} toml_time_local_t;
-
-// Represents a date and time with no offset from an absolute time (like UTC)
-// Actual date and time are interpreted by user or system
-typedef struct toml_datetime_local {
-	// Normal C date struct with time included
-	struct tm date;
-
-	// Nano seconds into the current second
-	uint32_t nano;
-} toml_datetime_local_t;
-
-// Constructs the memory (struct, array, etc) representing the toml structure, 
-// which could be any of the types above.
-// Should return a non-null pointer to that data or something else, or NULL
-// upon failure
-typedef void *(toml_ctor_fn)(const toml_t *parent, void *user);
-
-// toml value constructors
-// These return true if they pass, false if they don't
-#define TY(ename, cname, ty) \
-	typedef bool (toml_##cname##_fn)(void *data, const toml_t *in, \
-					void *user, const ty val);
-#define TY_SPECIAL(ename)
-	TOML_TYPES
-#undef TY
-#undef TY_SPECIAL
-
+typedef struct toml toml_t;
 typedef struct toml_val toml_val_t;
+typedef struct toml_res toml_res_t;
+typedef struct toml_table_info toml_table_info_t;
+typedef struct toml_array_info toml_array_info_t;
 
-// A union of constructors for different toml types
-typedef union toml_val_data {
-	// These functions simply set the data for the current table/array
-	toml_string_fn *set_string;
-	toml_int_fn *set_int;
-	toml_float_fn *set_float;
-	toml_bool_fn *set_bool;
-	toml_datetime_unix_fn *set_datetime_unix;
-	toml_datetime_local_fn *set_datetime_local;
-	toml_date_local_fn *set_date_local;
-	toml_time_local_fn *set_time_local;
+struct toml_table_info {
+	// Custom data
+	void *data;
 
-	// Refrence to the inner array types
-	const toml_val_t *array_start;
+	// Leave NULL to signify error
+	// This points to key-value pairs of the table ordered
+	// alphebetically by key name
+	const toml_t *start;
 
-	// Refrence to a table
-	// The kv pairs in here must be ordered alphebetically by key name
-	const toml_t *table_start;
-} toml_val_data_t;
+	// Length of table array
+	// If this is 0, then this table can have a variable amount of kv-pairs
+	size_t len;
+
+	// Used internally, no need to allocate room for it
+	void *_kv[];
+};
+struct toml_array_info {
+	// Custom data
+	void *data;
+
+	// Leave NULL to signify error
+	const toml_val_t *type;
+
+	// Maximum length of the array (0 for flexible sized arrays)
+	uint32_t cap;
+
+	// Used internally, no need to allocate room for it
+	uint32_t _len;
+};
+
+typedef bool toml_load_string_fn(void *parent_data, const char *val);
+typedef bool toml_load_int_fn(void *parent_data, int64_t val);
+typedef bool toml_load_float_fn(void *parent_data, double val);
+typedef bool toml_load_bool_fn(void *parent_data, bool val);
+typedef bool toml_load_datetime_unix_fn(void *parent_data, struct timespec val);
+typedef bool toml_load_datetime_fn(void *parent_data, struct tm datetime, uint32_t nano);
+typedef bool toml_load_date_fn(void *parent_data, struct tm date);
+typedef bool toml_load_time_fn(void *parent_data, uint32_t hour, uint32_t min,
+				struct timespec sec);
+typedef void toml_load_any_fn(void *parent_data, toml_type_t type, void *val);
+typedef toml_table_info_t toml_load_table_fn(void *parent_data);
+typedef toml_array_info_t toml_load_array_fn(void *parent_data);
 
 struct toml_val {
-	// Setter / data associated with this type
-	toml_val_data_t data;
-
-	// Length of array / number of kv pairs in table
-	uint32_t len;
-
-	// Leave this to 0, this is a generational boolean for if this value
-	// has been visited or not yet
-	uint16_t _parsed_gen;
-
-	// Length of the pattern for the array type
-	uint16_t pattern_len : 11;
-
-	// Type of the value
-	uint16_t type : 4;
-
-	// NOTE: Only used in kv pair inside of table
-	// Specifies if this value is optional
-	uint16_t isoptional : 1;
+	toml_type_t type;
+	bool optional;
+	union {
+		toml_load_string_fn *load_string;
+		toml_load_int_fn *load_int;
+		toml_load_float_fn *load_float;
+		toml_load_bool_fn *load_bool;
+		toml_load_datetime_unix_fn *load_datetime_unix;
+		toml_load_datetime_fn *load_datetime;
+		toml_load_date_fn *load_date;
+		toml_load_time_fn *load_time;
+		toml_load_any_fn *load_any;
+		toml_load_table_fn *load_table;
+		toml_load_array_fn *load_array;
+	};
 };
 
-// Maximum number of bytes that a toml key can be (including null-terminator)
-#define TOML_MAX_KEY_SIZE 32
-
-// Key/value type in a toml file
 struct toml {
-	// Name of this key
 	const char *name;
-
-	// What function gets run before this type is parsed?
-	toml_ctor_fn *ctor;
-
-	// Leave this to 0
-	uint32_t _num_parsed;
-
-	// Set this to the number of values that must be parsed
-	// (number of values that are not optional in this table)
-	uint32_t num_needed;
-
-	// Value of the key-value pair
-	toml_val_t val;
+	const toml_val_t val;
 };
 
-// Result of an toml parsing operation
-typedef struct toml_result {
-	// Did everything parse okay?
-	uint64_t okay : 1;
+struct toml_res {
+	// Did an error occur?
+	bool ok;
 
-	uint64_t _completely_parsed : 1;
+	// Where in the source code the error occurred
+	uint32_t line, pos;
 
-	// If not, here is the line it failed on
-	uint64_t line : 62;
-
-	// Parsing failed here in this part of the schema
-	const toml_t *in;
-
-	// Optional message explaining what happened
+	// Diagnosis info for the error
 	const char *msg;
-} toml_result_t;
+};
 
-// Structure to be passed into toml_parse
-typedef struct toml_args {
-	// The source code
-	const uint8_t *src;
-	
-	// Root toml table
-	size_t root_len;
-	toml_t *root_table;
-
-	// Optional user data
-	void *user;
-
-	// Since the parser uses a generation count to check if a value has
-	// been parsed, there needs to be a pointer to the current generation,
-	// this is also useful because then if the generation overflows, the
-	// parser will reset all generation counts (at a cost of time).
-	uint16_t *gen;
-} toml_args_t;
-
-// Parses a toml file using schema and calls the parsing functions in the
-// schema with the user_data parameter.
-// NOTE: the schema 
-toml_result_t toml_parse(const toml_args_t args);
+toml_res_t toml_parse(const char *src,
+			const toml_table_info_t schema_root,
+			void *arena, // If NULL, arena_len bytes are alloca'd
+			size_t arena_len); // If 0, default is 4kb
 
 #endif
 
