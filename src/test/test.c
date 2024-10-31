8bab42872b6c1ae5c8fa92de80655f2e450b57e9
Wyatt Radkiewicz <wyattwradkiewicz@gmail.com>
2024-10-31 13:56:15 -0600

    Moving number parser to own library

--- src/test/test.c
+++ src/test/test.c
@@ -784,71 +784,6 @@ static void test_ejstr_speed(void) {
 	}
 }
 
-static void test_ejint_speed(void) {
-	static const char *const strings[] = {
-		"0", "-1", "1234", "-1234", "12345678", "-12345678",
-		"9223372036854775807", "-9223372036854775808",
-		"9223372036854775900", "-9223372036854775900",
-	};
-	static const int64_t numbers[] = {
-		0, -1, 1234, -1234, 12345678, -12345678,
-		INT64_MAX, INT64_MIN, INT64_MAX, INT64_MIN,
-	};
-
-	static const int niters = 10000000, nums = 10;
-
-	// Calculate number of bytes
-	double ngigs = 0;
-	for (int j = 0; j < nums; j++) ngigs += strlen(strings[j]);
-	ngigs *= niters;
-	ngigs /= 1024 * 1024 * 1024;
-
-	// test functions
-	clock_t start;
-	double time;
-	printf("\n\nejint tests\n");
-
-	// test strtoll
-	start = clock();
-	for (int i = 0; i < niters; i++) {
-		for (int j = 0; j < nums; j++) {
-			strtoll(strings[j], NULL, 10);
-		}
-	}
-	time = (double)(clock() - start) / (double)CLOCKS_PER_SEC;
-	printf("strtoll %d iters time (s): %.4f\n", niters, time);
-	printf("strtoll throughput (GB/s): %.2f\n", ngigs / time);
-	printf("strtoll throughput (millions N/s): %.2f\n",
-		((double)(niters * nums) / 1000000.0) / time);
-	
-	// test atoll
-	start = clock();
-	volatile uint64_t n = 0;
-	for (int i = 0; i < niters; i++) {
-		for (int j = 0; j < nums; j++) {
-			n += atoll(strings[j]);
-		}
-	}
-	time = (double)(clock() - start) / (double)CLOCKS_PER_SEC;
-	printf("atoll %d iters time (s): %.4f\n", niters, time);
-	printf("atoll throughput (GB/s): %.2f\n", ngigs / time);
-	printf("atoll throughput (millions N/s): %.2f\n",
-		((double)(niters * nums) / 1000000.0) / time);
-
-	// test ejint
-	start = clock();
-	for (int i = 0; i < niters; i++) {
-		for (int j = 0; j < nums; j++) {
-			ejint(strings[j]);
-		}
-	}
-	time = (double)(clock() - start) / (double)CLOCKS_PER_SEC;
-	printf("ejint %d iters time (s): %.4f\n", niters, time);
-	printf("ejint throughput (GB/s): %.2f\n", ngigs / time);
-	printf("ejint throughput (millions N/s): %.2f\n",
-		((double)(niters * nums) / 1000000.0) / time);
-}
-
 static const test_t tests[] = {
 	TEST_ADD(pass_nothing)
 	TEST_PAD
@@ -1011,7 +946,7 @@ void usage(void) {
 }
 
 int main(int argc, char **argv) {
-	double d = ejflt("1.234");
+	double d = ejflt("1.234e-10");
 	printf("%20.20f\n", d);
 	printf("%llX\n", *(uint64_t *)&d);
 	return 0;
@@ -1037,7 +972,6 @@ int main(int argc, char **argv) {
 		? 0 : -1;
 	if (speed_test) {
 		//test_ejstr_speed();
-		test_ejint_speed();
 	}
 	return res;
 }
